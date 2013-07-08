6ada4d9e0f26a434921ecfe55bbb7260083240d8
Seecr Development Team <development@seecr.nl>
2013-07-08 12:00:46 +0200

    HM/JPM: added 'Stopping service' notification

--- seecr/test/integrationtestcase.py
+++ seecr/test/integrationtestcase.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 ## begin license ##
-# 
-# "Seecr Test" provides test tools. 
-# 
+#
+# "Seecr Test" provides test tools.
+#
 # Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+#
 # This file is part of "Seecr Test"
-# 
+#
 # "Seecr Test" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Seecr Test" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Seecr Test"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from __future__ import with_statement
@@ -71,10 +71,10 @@ class IntegrationState(object):
 
     def addToTestRunner(self, testRunner):
         testRunner.addGroup(
-            self.stateName, 
+            self.stateName,
             self.__tests,
             state=self)
-    
+
     def binDir(self):
         raise NotImplementedError()
 
@@ -179,8 +179,9 @@ class IntegrationState(object):
 
     def tearDown(self):
         for serviceName in self.pids.keys():
+            self._stdoutWrite("Stopping service '%s' for state '%s'." % (serviceName, self.stateName))
             self._stopServer(serviceName)
-    
+
     @staticmethod
     def _stdoutWrite(aString):
         stdout.write(aString)
