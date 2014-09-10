6d52dbcddc070e7791aa9718834b3f6052228ec8
Thijs Janssen <thijs@seecr.nl>
2014-09-10 10:04:54 +0200

    TJ: added test for observable.call on a CallTrace

--- test/calltracetest.py
+++ test/calltracetest.py
@@ -1,32 +1,33 @@
 ## begin license ##
-# 
-# "Seecr Test" provides test tools. 
-# 
+#
+# "Seecr Test" provides test tools.
+#
 # Copyright (C) 2005-2009 Seek You Too (CQ2) http://www.cq2.nl
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# Copyright (C) 2012, 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+#
 # This file is part of "Seecr Test"
-# 
+#
 # "Seecr Test" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Seecr Test" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Seecr Test"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from unittest import TestCase
 from types import GeneratorType
 from seecr.test import CallTrace
 from seecr.test.calltrace import TracedCall
+from meresco.core import Observable
 
 
 class CallTraceTest(TestCase):
@@ -188,3 +189,20 @@ class CallTraceTest(TestCase):
         calltrace.methodOne('aap')
         calltrace.methodTwo('aap')
         self.assertEquals(['methodOne', 'methodTwo'], calltrace.calledMethodNames())
+
+    def testObservableCall(self):
+        o = Observable()
+        calltrace = CallTrace('calltrace')
+        o.addObserver(calltrace)
+
+        result = o.call.getSomething()
+        self.assertEqual(None, result)
+        self.assertEquals(['getSomething'], calltrace.calledMethodNames())
+        calltrace.calledMethods.reset()
+
+        calltrace.returnValues['getSomething'] = 'RESULT'
+        result = o.call.getSomething()
+        self.assertEqual('RESULT', result)
+        self.assertEquals(['getSomething'], calltrace.calledMethodNames())
+
+
